1a72324835d8dde2834d681821191a34cc9be4fc
Dener Jamill <dener.jamill@gmail.com>
2020-03-05 10:48:53 -0300

    Adição da função problema e documentação.
    Adição da função problema e documentação das funções.

--- trabIC.py
+++ trabIC.py
@@ -1,22 +1,53 @@
 def remedio(txt):
+    """
+    Essa função recebe uma doença da lista 'medico' e retorna um texto com a doença
+    e o remédio a ser tomado.
+    :param txt: Palavra(s) presente(s) na lista 'medico' encontrado(s) na leitura do arquivo txt.
+    :return: Texto com a doença e o remédio a ser tomado.
+    """
+    rem = ''
     if txt.lower() in 'febre, dor de cabeça':
-        print(f'Você está com {txt.upper()}? Tome DIPIRONA')
+        rem = 'DIPIRONA'
     elif txt.lower() in 'gripe':
-        print(f'Você está com {txt.upper()}? Tome BENEGRIPE')
+        rem = 'BENEGRIPE'
+    return f'Você está com {txt.upper()}? Tome {rem.upper()}!'
 
 
-def outro(txt):
-    pass
+def problema(txt):
+    """
+    Essa função recebe um problema da lista 'computador' e retorna um texto com
+    o problema e uma provável solução.
+    :param txt: Palavra(s) presente(s) na lista 'computador' encontrada(s) na leitura do arquivo txt.
+    :return: Texto com o problema apresentado no pc e a indicação da provável peça defeituosa.
+    """
+    peca = ''
+    if txt.lower() in 'não liga':
+        peca = 'fonte ou alimentação'
+    elif txt.lower() in 'tela azul':
+        peca = 'memória ou registro'
+    elif txt.lower() in 'relógio errado':
+        peca = 'bateria'
+    return f'O seu PC está com problema de {txt.upper()}? Então o problema deve estar na(o) {peca.upper()} do seu pc!'
 
 
 def funcao(arquivo):
-    arquivo = open(arquivo + '.txt', 'r+')
+    """
+    Essa função recebe um arquivo txt, e a partir da leitura, indica qual a função a ser usada.
+    :param arquivo: Arquivo txt com o texto a ser lido.
+    :return: A requisição e sua solução.
+    """
+    arquivo = open(arquivo + '.txt', 'r')
     texto = arquivo.read()
     medico = ['gripe', 'febre', 'dor de cabeça']
+    computador = ['não liga', 'tela azul', 'relógio errado']
     for i in range(len(medico)):
         if medico[i] in texto.lower():
             print(f'Tem a palavra {medico[i].upper()}')
-            remedio(medico[i])
+            print(remedio(medico[i]))
+        if computador[i] in texto.lower():
+            print(f'Tem a plavra {computador[i].upper()}!')
+            print(problema(computador[i]))
+    arquivo.close()
 
 
 funcao('teste')
