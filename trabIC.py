5ba60d7ace37030fd13646a256d283abf1a6b164
Dener Jamill <dener.jamill@gmail.com>
2020-03-05 09:59:57 -0300

    Adição da função remedio()
    Adição da primeira função de teste remedio().

--- trabIC.py
+++ trabIC.py
@@ -1,10 +1,22 @@
+def remedio(txt):
+    if txt.lower() in 'febre, dor de cabeça':
+        print(f'Você está com {txt.upper()}? Tome DIPIRONA')
+    elif txt.lower() in 'gripe':
+        print(f'Você está com {txt.upper()}? Tome BENEGRIPE')
+
+
+def outro(txt):
+    pass
+
+
 def funcao(arquivo):
     arquivo = open(arquivo + '.txt', 'r+')
     texto = arquivo.read()
-    medico = ['GRIPE', 'FEBRE']
+    medico = ['gripe', 'febre', 'dor de cabeça']
     for i in range(len(medico)):
-        if medico[i] in texto.upper():
-            print(f'Tem a palavra {medico[i]}')
+        if medico[i] in texto.lower():
+            print(f'Tem a palavra {medico[i].upper()}')
+            remedio(medico[i])
 
 
 funcao('teste')
